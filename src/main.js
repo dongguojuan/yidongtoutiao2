a1baeadca667b4117b122c6094bd25862dc02fbd
dongguojuan <1395040604@qq.com>
2021-01-08 15:20:39 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import router from './router'
 import store from './store'
 // 加载全局样式
 import './styles/index.less'
+// 引入vant组件库和css
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
-
+// 注册vant
+Vue.use(Vant)
 new Vue({
   router,
   store,
